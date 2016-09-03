6776cf63e6d5f95a1d22b03e804e76ec4b25a766
ssniu <ssniu2011@gmail.com>
2016-09-03 15:17:43 -0400

    sign up text fix

---
+++ db/migrate/20160903190020_add_field_to_user.rb
@@ -0,0 +1,5 @@
+class AddFieldToUser < ActiveRecord::Migration
+  def change
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903170047) do
+ActiveRecord::Schema.define(version: 20160903190020) do
 
   create_table "homes", force: :cascade do |t|
     t.string   "name"
