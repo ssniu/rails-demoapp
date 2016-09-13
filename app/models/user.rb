5ac81008eca13a8de370646f180032bd85e00777
ssniu <ssniu2011@gmail.com>
2016-09-12 21:05:50 -0400

    new view

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,5 @@ class User < ActiveRecord::Base
                         with: /a-zA-Z0-9_-/,
                         message: 'Must be formate correctly.'
                      }
-            
+
 end
